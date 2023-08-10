da0933848dcc2e74f34eded0fd2f08d6e6e658a0
asif-mehmood@hotmail.com <asif-mehmood@hotmail.com>
2023-08-11 01:19:18 +0500

    complete front and backend

---
+++ calender-event-frontend/components/Event.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Event = ({ event }) => {
+  return (
+    <div>
+      <p
+        className={`relative truncate text-ellipsis rounded border border-[#7EB87D] bg-[#D4FCC9] py-1.5 pr-1 pl-5 text-base font-normal text-black`}
+      >
+        {event.name}
+        <span className="absolute top-1/2 left-[6px] h-[70%] w-[4px] translate-y-[-50%] rounded bg-[#7EB87D]" />
+      </p>
+    </div>
+  )
+}
+
+export default Event
